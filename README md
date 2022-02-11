dee489c0b7779a3d4d3f335d11f1ec45b982b9f4
hobbycoder7 <jameskopacz@yahoo.com>
2022-02-10 20:03:53 -0600

    Initial commit

---
+++ README md
@@ -0,0 +1,18 @@
+# Pandora.com desktop player hotkeys userscript  
+
+## Keys
+- J = Thumbs Down  
+- K = Tired of Track  
+- L = Thumbs Up  
+- N = Next Track  
+
+Only works on Now Playing page.  
+
+For install information visit [Violentmonkey](https://violentmonkey.github.io/get-it/).  
+
+---
+
+### Change log  
+
+Version 1.0
+- initial release  
---
+++ UserScript.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        Pandora.com desktop player hotkeys
+// @namespace   Violentmonkey Scripts
+// @match       https://www.pandora.com/*
+// @grant       none
+// @version     1.0
+// @author      hobbycoder7
+// @description keyboard short cuts to thumbs down, tired of track, thumbs up, and next track. J K L and N.
+// ==/UserScript==
+
+
+(function () {
+  document.addEventListener("keydown", function(e) {
+    if (document.querySelector('.nowPlayingTopInfo__current__icon button') != null) { // so hotkeys only work on Now Playing Screen. 
+      if (e.key === "j" || e.key === "J") { 
+        // thumbs down
+        document.querySelector('.ThumbDownButton.Tuner__Control__Button.Tuner__Control__ThumbDown__Button').click();
+      } else if (e.key === "k" || e.key === "K") { 
+        // tired of track
+        document.querySelector('.nowPlayingTopInfo__current__icon button').click();
+        document.querySelector("[data-qa='source_card_tired_of_track']").click();
+      } else if (e.key === "l" || e.key === "L") { 
+        // thumbs up
+        document.querySelector('.ThumbUpButton.Tuner__Control__Button.Tuner__Control__ThumbUp__Button').click();
+      } else if (e.key === "n" || e.key === "N") { 
+        // next track/skip current
+        document.querySelector('.SkipButton.Tuner__Control__Button.Tuner__Control__Skip__Button').click();
+      }
+    }
+  });
+})();
